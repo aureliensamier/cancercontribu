c281c11c4a38e8b99b14dae889086a90b5f4e924
Desespery <manon.salsou@eemi.com>
2019-11-28 14:51:21 +0100

    Header V.0

--- site/index.php
+++ site/index.php
@@ -23,7 +23,7 @@ mysqli_query($lien, 'set names utf8');*/
 	<input class="menu-btn" type="checkbox" id="menu-btn"/>
 	<label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
 	<ul class="menu">
-		<li><a href="#work">Our Work</a></li>
+		<li><a href="#work">Accueil</a></li>
 		<li><a href="#about">About</a></li>
 		<li><a href="#careers">Careers</a></li>
 		<li><a href="#contact">Contact</a></li>
