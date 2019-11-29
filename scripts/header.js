d5648372d6f238a8baf2d1950247837ffb9600fe
Desespery <manon.salsou@eemi.com>
2019-11-29 11:49:24 +0100

    Index footer + header v.2

--- scripts/header.js
+++ scripts/header.js
@@ -2,9 +2,7 @@ $(document).ready(function () {
 
     $(".header__inputMenu").click(function () {
         $(".header__menu").toggle();
-    })
-    $(".header__inputMenu").click(function () {
-        $("#menu-cross").toggle();
+        $("#menu-cross").toggleClass(".header__visibility");
     })
 
-});
+})
